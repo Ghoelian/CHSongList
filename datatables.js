3f3546932d06d37959ee27560c28e0275d72a222
Julian Vos <post@julianvos.nl>
2020-05-14 20:41:15 +0200

    Finish datatables stuff

--- datatables.js
+++ datatables.js
@@ -1,9 +1,16 @@
 $(document).ready(() => {
-  $.getJSON("https://julianvos.nl/assets/songs.json", (data) => {
-
-    $('#table').DataTable({
-      data: data
-    })
-  })
+	$.get("https://julianvos.nl/assets/songs.json", (data) => {
+		$('#table').DataTable({
+			data: data,
+			columns: [
+				{ data: "Name" },
+				{ data: "Artist" },
+				{ data: "Album" },
+				{ data: "Genre" },
+				{ data: "Charter" },
+				{ data: "Year" },
+				{ data: "Playlist" }
+			]
+		})
+	})
 })
-
